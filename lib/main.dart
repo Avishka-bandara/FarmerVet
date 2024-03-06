2920b019d9e86c15b19136360717d89a48d733f4
AvishkaBandara <21ug0225@sltc.edu.lk>
2024-03-06 21:16:54 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:farmervet/register.dart';
+
+void main() {
+  runApp(FarmerVetApp());
+}
+
+class FarmerVetApp extends StatefulWidget {
+  @override
+  State<FarmerVetApp> createState() => _FarmerVetAppState();
+}
+
+class _FarmerVetAppState extends State<FarmerVetApp> {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'FarmerVet',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController mobileController = TextEditingController();
+
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Flexible(
+              flex: 3,
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
+                child: Image.asset(
+                  'assets/cow.jpg', // Replace this with your image path
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(height: 25),
+            Text(
+              'Hi, Welcome to FarmerVet!',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+            Flexible(
+                flex: 2,
+                child: Column(children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    child: TextField(
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Mobile Number',
+                        prefixIcon: Icon(Icons.phone),
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.lock),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Login button onPressed logic
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                      fixedSize:
+                          const Size(300, 50), // Change the color as needed
+                    ),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  GestureDetector(
+                    onTap: () {
+                      Get.to(RegisterScreen());
+                    },
+                    child: Text(
+                      'Don\'t have an account? Sign Up',
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                ])),
+          ],
+        ),
+      ),
+    );
+  }
+}
