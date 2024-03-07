c6f99df36c1b632a2f3f8a4607428f547e2c4f67
AvishkaBandara <21ug0225@sltc.edu.lk>
2024-03-07 11:01:33 +0530

    Create animalDetail.dart

---
+++ lib/animalDetail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimalDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animal Detail'),
+      ),
+      body: Center(
+        child: Text('Animal Detail Screen'),
+      ),
+    );
+  }
+}
