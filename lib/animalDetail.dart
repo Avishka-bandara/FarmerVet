fa90a74fd0196588e8df1c334c51a6a8685a9983
AvishkaBandara <21ug0225@sltc.edu.lk>
2024-03-08 09:34:48 +0530

    Update animalDetail.dart

--- lib/animalDetail.dart
+++ lib/animalDetail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:farmervet/reportAnimal.dart';
 
 class AnimalDetail extends StatelessWidget {
   @override
@@ -9,34 +10,34 @@ class AnimalDetail extends StatelessWidget {
         title: Text('Animal Detail'),
         centerTitle: true,
       ),
-      body: Container(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      body: Padding(
+        padding: EdgeInsets.all(8.0), // Add padding to the whole screen
+        child: ListView(
           children: [
             CustomCardWidget(),
             SizedBox(height: 10),
-            Center(
-              // Wrap the Text widget with Center
-              child: Text(
-                'Record the vaccination given to the animal',
-                textAlign: TextAlign.center,
-              ),
+
+            Text(
+              'Record the vaccination given to the animal',
+              textAlign: TextAlign.center,
             ),
             SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                // Logic for adding vaccination details
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Color.fromRGBO(28, 42, 58, 1),
-                fixedSize: const Size(200, 36), // Change the size as needed
-              ),
-              child: const Text(
-                'Add Vaccination Details',
-                style: TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.white,
+            Align(
+              alignment: Alignment.centerLeft,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Logic for adding vaccination details
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromRGBO(28, 42, 58, 1),
+                  fixedSize: const Size(200, 36),
+                ),
+                child: const Text(
+                  'Add Vaccination Details',
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
@@ -47,7 +48,27 @@ class AnimalDetail extends StatelessWidget {
                 style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
             SizedBox(height: 10),
             onIssue(),
+            SizedBox(height: 50), // Adjust the height as needed for spacing
+            Divider(thickness: 1.0),
           ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 50,
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: ElevatedButton(
+            onPressed: () {
+              Get.to(ReportAnimal()); // Navigate to the Add Issue screen
+            },
+            style: ElevatedButton.styleFrom(
+              primary: Colors.black,
+            ),
+            child: Text(
+              'Add New Issue',
+              style: TextStyle(fontSize: 16.0, color: Colors.white),
+            ),
+          ),
         ),
       ),
     );
@@ -62,11 +83,10 @@ class CustomCardWidget extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.all(10.0),
         child: Row(
-          //crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              width: 100,
-              height: 100,
+              width: 130,
+              height: 130,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10.0),
                 image: DecorationImage(
@@ -78,7 +98,6 @@ class CustomCardWidget extends StatelessWidget {
             ),
             SizedBox(width: 10),
             Expanded(
-              // Wrap the Column with Expanded
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -100,7 +119,7 @@ class CustomCardWidget extends StatelessWidget {
                         child: const Text(
                           'Remove Animal',
                           style: TextStyle(
-                            fontSize: 10.1,
+                            fontSize: 12,
                             color: Color.fromRGBO(255, 255, 255, 1),
                           ),
                         ),
@@ -129,8 +148,6 @@ class CustomCardWidget extends StatelessWidget {
   }
 }
 
-/// grid view for On Going Issues
-
 class onIssue extends StatefulWidget {
   @override
   State<onIssue> createState() => _onIssueState();
@@ -139,80 +156,82 @@ class onIssue extends StatefulWidget {
 class _onIssueState extends State<onIssue> {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      // Wrap with Expanded
-      child: GridView.count(
-        crossAxisCount: 1, // Number of columns in the grid
-        mainAxisSpacing: 8.0, // Spacing between rows
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Health Issue',
-                            style: TextStyle(fontSize: 16),
+    return GridView.count(
+      crossAxisCount: 1,
+      mainAxisSpacing: 8.0,
+      shrinkWrap:
+          true, // Add this to allow GridView to scroll inside SingleChildScrollView
+      physics: NeverScrollableScrollPhysics(), // Prevent scrolling of GridView
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Health Issue',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Logic for the button
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Color.fromRGBO(254, 176, 82, 1),
                           ),
-                          ElevatedButton(
-                            onPressed: () {
-                              // Logic for the button
-                            },
-                            style: ElevatedButton.styleFrom(
-                              primary: Color.fromRGBO(254, 176, 82, 1),
-                            ),
-                            child: Text(
-                              'Minnor Issue',
-                              style: TextStyle(
-                                fontSize: 14.0,
-                                color: Colors.white,
-                              ),
+                          child: Text(
+                            'Minnor Issue',
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.white,
                             ),
                           ),
-                        ],
-                      )
-                    ],
-                  ),
+                        ),
+                      ],
+                    ),
+                    Text('Date'),
+                  ],
                 ),
               ),
-              SizedBox(height: 10),
-              Text('Date'),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // Logic for the first button
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Color.fromRGBO(234, 67, 53, 1),
-                      fixedSize: Size(120, 15),
-                    ),
-                    child: Text('Button 1'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Logic for the first button
+                  },
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(180, 20),
+                    primary: Color.fromRGBO(28, 42, 58, 1),
                   ),
-                  OutlinedButton(
-                    onPressed: () {
-                      // Logic for the second button
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Color.fromRGBO(234, 67, 53, 1),
-                      fixedSize: Size(120, 15),
-                    ),
-                    child: Text('Button 2'),
+                  child: Text('Now Healthy',
+                      style: TextStyle(color: Colors.white)),
+                ),
+                OutlinedButton(
+                  onPressed: () {
+                    // Logic for the second button
+                  },
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(180, 20),
                   ),
-                ],
-              ),
-            ],
-          ),
-        ],
-      ),
+                  child: Text(
+                    'Comment',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
