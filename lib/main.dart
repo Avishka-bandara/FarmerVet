c71d451523701f3dbfa64efdb7bfe4c6b9c289e5
AvishkaBandara <21ug0225@sltc.edu.lk>
2024-03-08 22:54:47 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:ui';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:farmervet/register.dart';
 
@@ -24,7 +20,6 @@ class _FarmerVetAppState extends State<FarmerVetApp> {
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      debugShowCheckedModeBanner: false,
       home: LoginScreen(),
     );
   }
@@ -39,133 +34,102 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController mobileController = TextEditingController();
 
   final TextEditingController passwordController = TextEditingController();
-
-
   @override
   Widget build(BuildContext context) {
-    final Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: screenSize.height,
-          width: screenSize.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.only(bottomLeft:Radius.circular(15),bottomRight:Radius.circular(15) ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Flexible(
+              flex: 3,
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
                 child: Image.asset(
                   'assets/cow.jpg', // Replace this with your image path
                   fit: BoxFit.cover,
                 ),
               ),
-              SizedBox(height: 25),
-              Text(
-                'Hi, Welcome to FarmerVet',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromRGBO(28, 42, 58, 1)
-                ),
+            ),
+            SizedBox(height: 25),
+            Text(
+              'Hi, Welcome to FarmerVet!',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(height: 40),
-              Column(children: [
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(12, 16, 12, 16),
-                  child: SizedBox(
-                    height: 45,
-                    width: 342,
+            ),
+            SizedBox(height: 30),
+            Flexible(
+                flex: 2,
+                child: Column(children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
                     child: TextField(
-                      style: TextStyle(fontSize: 15),
                       keyboardType: TextInputType.phone,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
-                        labelText: 'Registered Phone Number',
-                        prefixIcon: Icon(Icons.local_phone_outlined),
-                        hintStyle: TextStyle(fontSize: 12),
+                        labelText: 'Mobile Number',
+                        prefixIcon: Icon(Icons.phone),
                       ),
                     ),
                   ),
-                ),
-
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(12, 16, 12, 16),
-                  child: SizedBox(
-                    height: 45,
-                    width: 342,
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
                     child: TextField(
                       obscureText: true,
                       decoration: InputDecoration(
                         labelText: 'Password',
                         border: OutlineInputBorder(),
-                        prefixIcon: Icon(Icons.lock_outline),
+                        prefixIcon: Icon(Icons.lock),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 60),
-                SizedBox(
-                  height: 48,
-                  width: 342,
-                  child: ElevatedButton(
+                  SizedBox(height: 15),
+                  ElevatedButton(
                     onPressed: () {
                       // Login button onPressed logic
                     },
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(28, 42, 58, 1),
+                      primary: Colors.green,
                       fixedSize:
                           const Size(300, 50), // Change the color as needed
                     ),
                     child: const Text(
                       'Login',
                       style: TextStyle(
-                        fontSize: 16.0,
+                        fontSize: 18.0,
                         fontWeight: FontWeight.bold,
                         color: Colors.white,
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 85),
-                Text(
-                  'Forgot Password?',
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w500,
-                    color: Color.fromRGBO(28, 100, 242, 1),
+                  SizedBox(height: 15),
+                  Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
                   ),
-                ),
-                SizedBox(height: 15),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Don't have an account?  ",
+                  SizedBox(height: 15),
+                  GestureDetector(
+                    onTap: () {
+                      Get.to(RegisterScreen());
+                    },
+                    child: Text(
+                      'Don\'t have an account? Sign Up',
                       style: TextStyle(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w400,
-                        color: Color.fromRGBO(107, 114, 128, 1),
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
                       ),
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        Get.to(RegisterScreen());
-                      },
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w500,
-                          color: Color.fromRGBO(28, 100, 242, 1),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-
-              ]),
-            ],
-          ),
+                  ),
+                ])),
+          ],
         ),
       ),
     );
