d325ef44d437fffe625c9ff1048df304d7083852
Bawantha Wijekoon <wrbawantha@gmail.com>
2024-03-13 17:46:11 +0530

    Firebase connected

--- lib/farmer_registration.dart
+++ lib/farmer_registration.dart
@@ -1,6 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:farmervet/farmer_animal.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:farmervet/add_animal.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
+import 'firebase_auth_services.dart';
 
 class UserRegistrationForm extends StatefulWidget {
   @override
@@ -8,10 +13,16 @@ class UserRegistrationForm extends StatefulWidget {
 }
 
 class _UserRegistrationFormState extends State<UserRegistrationForm> {
-  TextEditingController _nameController = TextEditingController();
-  TextEditingController _idController = TextEditingController();
-  TextEditingController _addressController = TextEditingController();
-  late String _selectedDivision = '';
+  final FirebaseAuthService _auth=FirebaseAuthService();
+  TextEditingController _farmnameController = TextEditingController();
+  TextEditingController _farmidController = TextEditingController();
+  TextEditingController _farmaddressController = TextEditingController();
+  TextEditingController _DivisionalSecretariatController = TextEditingController();
+  TextEditingController _gramaniladhariAreacontroller = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _password = TextEditingController();
+
+  bool isLoading=false;
 
   @override
   Widget build(BuildContext context) {
@@ -53,6 +64,7 @@ class _UserRegistrationFormState extends State<UserRegistrationForm> {
                         height: 45,
                         width: 342,
                         child: TextField(
+                          controller: _farmnameController,
                           decoration: InputDecoration(
                             filled: true,
                             fillColor: Color.fromRGBO(209, 213, 219, 1),
@@ -71,6 +83,7 @@ class _UserRegistrationFormState extends State<UserRegistrationForm> {
                         height: 45,
                         width: 342,
                         child: TextField(
+                          controller: _farmidController,
                           decoration: InputDecoration(
                             filled: true,
                             fillColor: Color.fromRGBO(209, 213, 219, 1), // Change this to the color you want
@@ -89,6 +102,7 @@ class _UserRegistrationFormState extends State<UserRegistrationForm> {
                         height: 45,
                         width: 342,
                         child: TextField(
+                          controller: _farmaddressController,
                           decoration: InputDecoration(
                             filled: true,
                             fillColor: Color.fromRGBO(209, 213, 219, 1),
@@ -100,17 +114,18 @@ class _UserRegistrationFormState extends State<UserRegistrationForm> {
                       ),
                     ),
 
-                    Text("Divisional Secretariat"),
+                    Text("Divisional Secretariat Area"),
                     Padding(
                       padding: EdgeInsets.fromLTRB(0, 0, 0, 25),
                       child: SizedBox(
                         height: 45,
                         width: 342,
                         child: TextField(
+                          controller: _DivisionalSecretariatController,
                           decoration: InputDecoration(
                             filled: true,
                             fillColor: Color.fromRGBO(209, 213, 219, 1),
-                            hintText: 'Divisional Secteriest',
+                            hintText: 'Divisional Secteriest Area',
                             border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(8))),
                             // prefixIcon: Icon(Icons.lock_outline),
                           ),
@@ -124,48 +139,32 @@ class _UserRegistrationFormState extends State<UserRegistrationForm> {
                       child: SizedBox(
                         height: 45,
                         width: 342,
-                        child: DropdownButtonFormField<String>(
+                        child: TextField(
+                          controller: _gramaniladhariAreacontroller,
                           decoration: InputDecoration(
                             filled: true,
                             fillColor: Color.fromRGBO(209, 213, 219, 1),
                             hintText: 'Grama Niladari Area',
                             border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(8))),
-                            //prefixIcon: Icon(Icons.lock_outline),
+                            // prefixIcon: Icon(Icons.lock_outline),
                           ),
-                          items: [
-                            DropdownMenuItem(
-                              child: Text('Kandy'),
-                              value: 'Option 1',
-                            ),
-                            DropdownMenuItem(
-                              child: Text('Udunuwara'),
-                              value: 'Option 2',
-                            ),
-                            DropdownMenuItem(
-                              child: Text('Peradeniya'),
-                              value: 'Option 3',
-                            ),
-                            // Add more DropdownMenuItem as needed
-                          ],
-                          onChanged: (String? value) {
-                            Text(value.toString());
-                          },
                         ),
                       ),
                     ),
 
-                    Text("Enter Phone Number"),
+                    Text("Enter Email Address"),
                     Padding(
                       padding: EdgeInsets.fromLTRB(0, 0, 0, 25),
                       child: SizedBox(
                         height: 45,
                         width: 342,
                         child: TextField(
-                          keyboardType: TextInputType.phone,
+                          controller: _emailController,
+                          keyboardType: TextInputType.emailAddress,
                           decoration: InputDecoration(
                             filled: true,
                             fillColor: Color.fromRGBO(209, 213, 219, 1),
-                            hintText: 'Phone Number',
+                            hintText: 'Email',
                             border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(8))),
                             // prefixIcon: Icon(Icons.lock_outline),
                           ),
@@ -180,6 +179,7 @@ class _UserRegistrationFormState extends State<UserRegistrationForm> {
                         height: 45,
                         width: 342,
                         child: TextField(
+                          controller: _password,
                           obscureText: true,
                           decoration: InputDecoration(
                             filled: true,
@@ -200,23 +200,14 @@ class _UserRegistrationFormState extends State<UserRegistrationForm> {
                   width: 342,
                   child: ElevatedButton(
                     onPressed: () {
-                      // Implement registration logic here
-                      Get.to(addAnimalForm());
-                      String name = _nameController.text;
-                      String id = _idController.text;
-                      String address = _addressController.text;
-                      String division = _selectedDivision;
-                      print('Name: $name');
-                      print('ID: $id');
-                      print('Address: $address');
-                      //print('Division: $division');
+                      checkSignup();
                     },
                     style: ElevatedButton.styleFrom(
                       primary: Color.fromRGBO(28, 42, 58, 1),
                       fixedSize: const Size(300, 40),
                     ),
                     child: Text(
-                      'Next',
+                      'Create an account',
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 16.0,
@@ -231,5 +222,80 @@ class _UserRegistrationFormState extends State<UserRegistrationForm> {
       ),
     );
   }
+
+  void signup (String email,String password,String farmname,String farmid,String farmaddress,String divisionalsec,String gramanila)async{
+
+    try{
+      User? user=await _auth.signUpWithEmailAndPassword(email, password);
+      if(user!=null){
+        showToast(user.uid);
+        setState(() {
+          isLoading=false;
+        });
+
+        await FirebaseFirestore.instance.collection('Farm details').doc(user.uid).set({
+          'User Id':user.uid,
+          'Farmer Email':email,
+          'Farm Name':farmname,
+          'Farm Id':farmid,
+          'Farm Address':farmaddress,
+          'Divisional secretariat area':divisionalsec,
+          'Grama niladari area':gramanila
+          // Add more fields as needed
+        });
+        showToast("Account Created Successfully");
+        Navigator.push(context,MaterialPageRoute(builder: (context)=> Home()));
+      }
+
+      else{
+        setState(() {
+          isLoading=false;
+        });
+        showToast("This account is already exists");
+      }
+    }catch(e){
+      print(e.toString());
+      showToast(e.toString());
+    }
+
+  }
+
+  void checkSignup(){
+    String farmname = _farmnameController.text;
+    String farmid = _farmidController.text;
+    String farmaddress = _farmaddressController.text;
+    String divisionalsec = _DivisionalSecretariatController.text;
+    String gramanila = _gramaniladhariAreacontroller.text;
+    String email = _emailController.text;
+    String password = _password.text;
+
+      if(!password.isEmpty && !farmname.isEmpty && !farmid.isEmpty && !farmaddress.isEmpty && !divisionalsec.isEmpty && !gramanila.isEmpty && !email.isEmpty){
+        if(password.length>=6){
+          setState(() {
+            isLoading=true;
+          });
+          signup(email,password,farmname,farmid,farmaddress,divisionalsec,gramanila);
+        }else{
+          showToast("Password must be include at least 6 characters");
+        }
+
+      }else{
+        showToast("All the field must be filled");
+      }
+  }
+
+  void showToast(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.black,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+  }
+
 }
+
 
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:farmervet/user_login.dart';
 
 Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: const FirebaseOptions(apiKey: "AIzaSyDwno0qdxzwfT5KRSqNSVnTswyNzwVGnqQ", appId: "1:93156038537:android:93c9059b01621461daf358", messagingSenderId: "93156038537", projectId: "farmervet-4e6b9"));
   await Firebase.initializeApp();
   runApp(FarmerVetApp());
 }
