7935f9e3db2cc6d01d2ee814d9837f299c1574d5
AvishkaBandara <21ug0225@sltc.edu.lk>
2024-03-15 12:43:21 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:farmervet/user_login.dart';
-
 import 'farmer_animal.dart';
 
-Future main() async{
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: const FirebaseOptions(apiKey: "AIzaSyDwno0qdxzwfT5KRSqNSVnTswyNzwVGnqQ", appId: "1:93156038537:android:93c9059b01621461daf358", messagingSenderId: "93156038537", projectId: "farmervet-4e6b9"));
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDwno0qdxzwfT5KRSqNSVnTswyNzwVGnqQ",
+          appId: "1:93156038537:android:93c9059b01621461daf358",
+          messagingSenderId: "93156038537",
+          projectId: "farmervet-4e6b9"));
   await Firebase.initializeApp();
 
   User? user = FirebaseAuth.instance.currentUser;
@@ -22,7 +25,7 @@ class FarmerVetApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: user != null ? Animal() : LoginScreen(),
     );
