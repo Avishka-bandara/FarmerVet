74acde54e6cbd3addf36cebfdfbafd69455a038a
AvishkaBandara <21ug0225@sltc.edu.lk>
2024-03-16 17:23:50 +0530

    added navigation in vet_farm_detail.dart

--- lib/vet_farm_detail.dart
+++ lib/vet_farm_detail.dart
@@ -1,13 +1,14 @@
 import 'package:farmervet/farmList.dart';
+import 'package:farmervet/farm_milk_output.dart';
 import 'package:farmervet/vet_animalissue.dart';
+import 'package:farmervet/vet_farm_breeding_info.dart';
 import 'package:flutter/material.dart';
 
 class FarmDetailView extends StatelessWidget {
-
   final List<Farm> farm;
   final int index;
 
-  FarmDetailView(this.farm,this.index);
+  FarmDetailView(this.farm, this.index);
 
   @override
   Widget build(BuildContext context) {
@@ -22,21 +23,28 @@ class FarmDetailView extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Farmer Name : '+farm[index].name,
+              'Farmer Name : ' + farm[index].name,
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
             Divider(thickness: 1),
             SizedBox(height: 10),
-            Text('Registration ID : '+farm[index].id, style: TextStyle(fontSize: 15.0)),
+            Text('Registration ID : ' + farm[index].id,
+                style: TextStyle(fontSize: 15.0)),
             SizedBox(height: 10),
-            Text('Address : '+farm[index].address, style: TextStyle(fontSize: 15.0)),
+            Text('Address : ' + farm[index].address,
+                style: TextStyle(fontSize: 15.0)),
             SizedBox(height: 10),
-            Text('Divisional Secretary : '+farm[index].divisionalSecretariatArea, style: TextStyle(fontSize: 15.0)),
+            Text(
+                'Divisional Secretary : ' +
+                    farm[index].divisionalSecretariatArea,
+                style: TextStyle(fontSize: 15.0)),
             SizedBox(height: 10),
-            Text('Gram Niladari Area : '+farm[index].area, style: TextStyle(fontSize: 15.0)),
+            Text('Gram Niladari Area : ' + farm[index].area,
+                style: TextStyle(fontSize: 15.0)),
             SizedBox(height: 10),
-            Text('Email Address: '+farm[index].email, style: TextStyle(fontSize: 15.0)),
+            Text('Email Address: ' + farm[index].email,
+                style: TextStyle(fontSize: 15.0)),
             SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -52,12 +60,28 @@ class FarmDetailView extends StatelessWidget {
                     child: Image.asset('assets/view.png'),
                   ),
                 ),
-                Container(
-                  child: Image.asset('assets/milk.png'),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => totalMilkOutput()),
+                    );
+                  },
+                  child: Container(
+                    child: Image.asset('assets/milk.png'),
+                  ),
                 ),
-                Container(
-                  child: Image.asset('assets/breed.png'),
-                )
+                GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => BreedingInfo()),
+                      );
+                    },
+                    child: Container(
+                      child: Image.asset('assets/breed.png'),
+                    )),
               ],
             )
           ],
