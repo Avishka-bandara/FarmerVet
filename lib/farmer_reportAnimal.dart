7ce1aadb76854bc27f64e3f9f4a6cc08001b4e89
AvishkaBandara <21ug0225@sltc.edu.lk>
2024-03-21 22:57:29 +0530

    push <-> pushreplacement

--- lib/farmer_reportAnimal.dart
+++ lib/farmer_reportAnimal.dart
@@ -12,10 +12,9 @@ import 'CowList.dart';
 import 'farmer_animal.dart';
 
 class ReportAnimal extends StatefulWidget {
-
   final List<Cow> cows;
   final int index;
-  ReportAnimal({required this.cows,required this.index});
+  ReportAnimal({required this.cows, required this.index});
 
   @override
   State<ReportAnimal> createState() => _ReportAnimalState();
@@ -25,13 +24,13 @@ class _ReportAnimalState extends State<ReportAnimal> {
   String _currentDateTime = '';
   bool isLoading = false;
   User? user = FirebaseAuth.instance.currentUser;
-  String? _selectedIssue='';
+  String? _selectedIssue = '';
   final TextEditingController issuecontroller = TextEditingController();
   String? _imagePath;
-  String imageUrl='';
-  bool isSelectedlimping=false;
-  bool isSelecteddiarhea=false;
-  bool isSelectedfever=false;
+  String imageUrl = '';
+  bool isSelectedlimping = false;
+  bool isSelecteddiarhea = false;
+  bool isSelectedfever = false;
 
   @override
   Widget build(BuildContext context) {
@@ -54,77 +53,88 @@ class _ReportAnimalState extends State<ReportAnimal> {
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
               SizedBox(height: 20),
-
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: isSelectedlimping?Colors.deepPurple:Colors.white
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: isSelectedlimping
+                                ? Colors.deepPurple
+                                : Colors.white),
+                        child: OutlinedButton(
+                          onPressed: () {
+                            setState(() {
+                              _selectedIssue = 'Limping';
+                              isSelectedlimping = true;
+                              isSelecteddiarhea = false;
+                              isSelectedfever = false;
+                            });
+                          },
+                          child: Text('Limping',
+                              style: isSelectedlimping
+                                  ? TextStyle(color: Colors.white)
+                                  : TextStyle()),
+                        ),
                       ),
-                      child: OutlinedButton(
-                        onPressed: (){
-                          setState(() {
-                            _selectedIssue = 'Limping';
-                            isSelectedlimping=true;
-                            isSelecteddiarhea=false;
-                            isSelectedfever=false;
-                          });
-                        },
-                        child: Text('Limping',style: isSelectedlimping?TextStyle(color: Colors.white):TextStyle()),
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: isSelecteddiarhea
+                                ? Colors.deepPurple
+                                : Colors.white),
+                        child: OutlinedButton(
+                          onPressed: () {
+                            setState(() {
+                              _selectedIssue = 'Diarrhea';
+                              isSelectedlimping = false;
+                              isSelecteddiarhea = true;
+                              isSelectedfever = false;
+                            });
+                          },
+                          child: Text('Diarrhea',
+                              style: isSelecteddiarhea
+                                  ? TextStyle(color: Colors.white)
+                                  : TextStyle()),
+                        ),
                       ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: isSelecteddiarhea?Colors.deepPurple:Colors.white
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: isSelectedfever
+                                ? Colors.deepPurple
+                                : Colors.white),
+                        child: OutlinedButton(
+                          onPressed: () {
+                            setState(() {
+                              _selectedIssue = 'Fever';
+                              isSelectedlimping = false;
+                              isSelecteddiarhea = false;
+                              isSelectedfever = true;
+                            });
+                          },
+                          child: Text('Fever',
+                              style: isSelectedfever
+                                  ? TextStyle(color: Colors.white)
+                                  : TextStyle()),
+                        ),
                       ),
-                      child: OutlinedButton(
-                        onPressed: () {
-                          setState(() {
-                            _selectedIssue = 'Diarrhea';
-                            isSelectedlimping=false;
-                            isSelecteddiarhea=true;
-                            isSelectedfever=false;
-                          });
-                        },
-                        child: Text('Diarrhea',style: isSelecteddiarhea?TextStyle(color: Colors.white):TextStyle()),
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: isSelectedfever?Colors.deepPurple:Colors.white
-                      ),
-                      child: OutlinedButton(
-                        onPressed: (){
-                          setState(() {
-                            _selectedIssue = 'Fever';
-                            isSelectedlimping=false;
-                            isSelecteddiarhea=false;
-                            isSelectedfever=true;
-                          });
-                        },
-                        child: Text('Fever',style: isSelectedfever?TextStyle(color: Colors.white):TextStyle()),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 5),
-              ],
-            ),
+                    ],
+                  ),
+                  SizedBox(height: 5),
+                ],
+              ),
               SizedBox(height: 10),
               TextField(
-                onTap: (){
+                onTap: () {
                   setState(() {
-                    _selectedIssue='';
-                    isSelectedlimping=false;
-                    isSelecteddiarhea=false;
-                    isSelectedfever=false;
+                    _selectedIssue = '';
+                    isSelectedlimping = false;
+                    isSelecteddiarhea = false;
+                    isSelectedfever = false;
                   });
                 },
                 controller: issuecontroller,
@@ -137,46 +147,45 @@ class _ReportAnimalState extends State<ReportAnimal> {
                 ),
               ),
               SizedBox(height: 20),
-
-          Container(
-            height: 150, // Adjust height as needed
-            width: double.infinity, // Take full width
-            child: Stack(
-              children: [
-                if (_imagePath != null)
-                  Stack(
-                    alignment: Alignment.topRight,
-                    children: [
-                      Image.file(
-                        File(_imagePath!),
-                        height: 100,
-                        width: 100,
+              Container(
+                height: 150, // Adjust height as needed
+                width: double.infinity, // Take full width
+                child: Stack(
+                  children: [
+                    if (_imagePath != null)
+                      Stack(
+                        alignment: Alignment.topRight,
+                        children: [
+                          Image.file(
+                            File(_imagePath!),
+                            height: 100,
+                            width: 100,
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.cancel_outlined),
+                            onPressed: () {
+                              setState(() {
+                                _imagePath = null;
+                              });
+                            },
+                          ),
+                        ],
                       ),
-                      IconButton(
-                        icon: Icon(Icons.cancel_outlined),
-                        onPressed: () {
-                          setState(() {
-                            _imagePath = null;
-                          });
-                        },
+                    if (_imagePath == null)
+                      ElevatedButton(
+                        onPressed: _pickImageFromGallery,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromRGBO(28, 42, 58, 1),
+                          fixedSize: const Size(200, 45),
+                        ),
+                        child: Text(
+                          'Add Image',
+                          style: TextStyle(fontSize: 16.0, color: Colors.white),
+                        ),
                       ),
-                    ],
-                  ),
-                if (_imagePath == null)
-                  ElevatedButton(
-                    onPressed: _pickImageFromGallery,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(28, 42, 58, 1),
-                      fixedSize: const Size(200, 45),
-                    ),
-                    child: Text(
-                      'Add Image',
-                      style: TextStyle(fontSize: 16.0, color: Colors.white),
-                    ),
-                  ),
-              ],
-            ),
-          ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
@@ -187,6 +196,8 @@ class _ReportAnimalState extends State<ReportAnimal> {
           padding: EdgeInsets.symmetric(horizontal: 16.0),
           child: ElevatedButton(
             onPressed: () {
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (context) => Animal()));
               register();
             },
             style: ElevatedButton.styleFrom(
@@ -194,30 +205,30 @@ class _ReportAnimalState extends State<ReportAnimal> {
             ),
             child: isLoading
                 ? const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Submitting....  ',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(
-                    height: 30,
-                    width: 30,
-                    child: CircularProgressIndicator(
-                      color: Colors.white,
-                    )),
-              ],
-            )
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Submitting....  ',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                          height: 30,
+                          width: 30,
+                          child: CircularProgressIndicator(
+                            color: Colors.white,
+                          )),
+                    ],
+                  )
                 : const Text(
-              'Submit',
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Colors.white,
-              ),
-            ),
+                    'Submit',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
+                  ),
           ),
         ),
       ),
@@ -236,31 +247,34 @@ class _ReportAnimalState extends State<ReportAnimal> {
 
   void _updateDateTime() {
     setState(() {
-      _currentDateTime = DateFormat('yyyy-MM-dd - HH:mm').format(DateTime.now());
+      _currentDateTime =
+          DateFormat('yyyy-MM-dd - HH:mm').format(DateTime.now());
     });
   }
 
   Future<void> register() async {
-    String uniqueFileName=DateTime.now().millisecondsSinceEpoch.toString();
-    Reference referenceRoot =FirebaseStorage.instance.ref();
-    Reference referenceDirImage=referenceRoot.child('images');
-    Reference referenceImageToUpload=referenceDirImage.child(uniqueFileName);
+    String uniqueFileName = DateTime.now().millisecondsSinceEpoch.toString();
+    Reference referenceRoot = FirebaseStorage.instance.ref();
+    Reference referenceDirImage = referenceRoot.child('images');
+    Reference referenceImageToUpload = referenceDirImage.child(uniqueFileName);
 
     _updateDateTime();
     setState(() {
       isLoading = true;
     });
 
-    String?issue='';
-    _selectedIssue==''?issue=issuecontroller.text: issue=_selectedIssue;
+    String? issue = '';
+    _selectedIssue == ''
+        ? issue = issuecontroller.text
+        : issue = _selectedIssue;
 
-    if(issue==''){
+    if (issue == '') {
       showToast("Please select or type a symptom");
       setState(() {
         isLoading = false;
       });
       return;
-    }else if(_imagePath==null){
+    } else if (_imagePath == null) {
       showToast("Please add an image");
       setState(() {
         isLoading = false;
@@ -268,27 +282,28 @@ class _ReportAnimalState extends State<ReportAnimal> {
       return;
     }
 
-    try{
-      await referenceImageToUpload.putFile(File(_imagePath!)).then((p0) => null);
-      imageUrl= await referenceImageToUpload.getDownloadURL();
-
-    }catch(error){
+    try {
+      await referenceImageToUpload
+          .putFile(File(_imagePath!))
+          .then((p0) => null);
+      imageUrl = await referenceImageToUpload.getDownloadURL();
+    } catch (error) {
       showToast(error.toString());
       print('Error uploading image: $error');
     }
 
     await FirebaseFirestore.instance
-        .collection('Farm details/' +user!.uid +'/health issue')
+        .collection('Farm details/' + user!.uid + '/health issue')
         .doc()
         .set({
-      'animal id':widget.cows[widget.index].id,
+      'animal id': widget.cows[widget.index].id,
       'animalissue': issue,
-      'timeDate':_currentDateTime,
-      'cowname':widget.cows[widget.index].name,
-      'cowtype':widget.cows[widget.index].type,
-      'cowage':widget.cows[widget.index].age,
-      'imageurl':imageUrl,
-      'visit':true,
+      'timeDate': _currentDateTime,
+      'cowname': widget.cows[widget.index].name,
+      'cowtype': widget.cows[widget.index].type,
+      'cowage': widget.cows[widget.index].age,
+      'imageurl': imageUrl,
+      'visit': true,
       // Add more fields as needed
     }).then((value) {
       setState(() {
@@ -297,7 +312,6 @@ class _ReportAnimalState extends State<ReportAnimal> {
       showToast("Saved");
       Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => Animal()));
-
     }).catchError((error) {
       setState(() {
         isLoading = false;
